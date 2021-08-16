96d81f78d27953b71831f28132139cec142cce42
jacqueline <jacqueline.preibisch@gmail.com>
2021-08-16 18:55:18 -0300

    Primer Ejercicio JS

---
+++ main.js
@@ -0,0 +1,9 @@
+let entrada = prompt ("Ingresa tu nombre");
+let salida = entrada + " " + "Bienvenido/a";
+alert (salida);
+
+var numeroA= "2";
+let numeroB= "4";
+let resultadoProducto= numeroA*numeroB;
+
+
