561412bc4931b612f351d6ed506ab2a2263e0902
jacqueline <jacqueline.preibisch@gmail.com>
2021-08-22 14:52:03 -0300

    Trabajo 2

--- main.js
+++ main.js
@@ -10,6 +10,24 @@ entrada= parseInt (entrada);
 let salida= entrada + (numeroA);
 alert (salida);
 
+// Trabajo 2//
+let edad= prompt ("Indique su edad");
+let nombre=  prompt ("Indique su nombre");
 
+
+if ((edad > 55) || (nombre== "joaquin")){alert ("Bienvenido Joaquin");
+}else{
+    alert("error: tu no eres joaquin");
+}
+
+// Trabajo 2 parte B//
+
+let partidos= prompt ("Indique cantidad de partidos jugados");
+let club=  prompt ("Indique su club")
+
+if ((partidos==15) && (nombre!="River")) {alert ("Ingresada");
+}else{
+    console.log ("Intenta nuevamente");
+}
 
 
