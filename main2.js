3f09c1fa9d0ca3d029ee0876de4cf16865be7fc8
jacqueline <jacqueline.preibisch@gmail.com>
2021-10-03 20:11:11 -0300

    trabajo final v1

--- main2.js
+++ main2.js
@@ -11,37 +11,47 @@
 
 //1. Función que reciba Nombre del servicio, Frecuencia  y Precio y dentro, cree el objeto ItemCarrito con esos datos y lo pushée dentro del array carrito
 class Servicio{
-    constructor (nombre, precio, frecuencia) {
+    constructor (nombre, precio, frecuencia, imagenURL) {
         this.id = id;
         this.nombre = nombre;
         this.precio = precio;
         this.frecuencia = frecuencia;
+        this.imagenURL = imagenURL;
     }
     mostrarMensaje() {
         alert (this.nombre + " tiene un valor de $" + this.precio);
     }
 }
+class ItemCarrito{
+    constructor (id) {
+        this.id = id;
+        this.cantidad = 1;
+    }
+}
 
-function crearServicios(id, nombre, precio, frecuencia){
-    return new Servicio(id, nombre, precio, frecuencia);
+function crearServicio(nombre, precio, frecuencia, imagenURL, arrayServicios){
+    let id = arrayServicios.length;
+    arrayServicios.push(new Servicio(id, nombre, precio, frecuencia, imagenURL));
 };
-function main(){
-// base de servicios hardcodeada
-const servicios = [];
-servicios.push(crearServicios(1,"Interconsulta", 1250, "unica"));
-servicios.push(crearServicios(2,"Tratamiento Cognitivo Conductual", 8900, "Regular"));
-servicios.push(crearServicios(3,"Sesion Psicologia", 3500, "unica"));
 
-let carrito = seleccionarServicios(servicios);
-mostrarCarrito(servicios);
+function crearTarjetas (arrayServicios){
+
+
+// base de servicios hardcodeada
+
+const listaServicios = [];
+crearServicio("Tratamiento Cognitivo Conductual", 8900, "Regular", "/imagenes/tratamiento.jpg", listaServicios);
+crearServicio("Interconsulta", 1250, "unica", "/imagenes/interconsulta.jpg", listaServicios);
+crearServicio("Sesion Psicologia", 3500, "unica", "/imagenes/sesion.jpg", listaServicios);
+
 }
 
 /* Array de servicios*/ 
 
-function seleccionarServicios(servicios) {
+function seleccionarServicios(listaServicios) {
     const carrito = [];
     alert ("Seleccione los servicios que desea agregar a su carrito");
-    for(servicio of servicios) {
+    for(servicio of listaServicios) {
         let seleccion = confirm (servicio.nombre + " tiene un costo de $" + servicio.precio + " y una frecuencia " + servicio.frecuencia + "\n ¿Desea agregarlo al carrito?");
         if (seleccion) {
             carrito.push(carrito);
@@ -50,27 +60,65 @@ function seleccionarServicios(servicios) {
     return carrito;
 }
 
-//1b otra similar para el stock pero que además le asigne a cada servicio un id numérico en base al largo del array servicios
-// 3. Función que cree las tarjetas de producto en base al array stock
-let contenedor = document.createElement ("div");
 
-contenedor.innerHTML = `<li> id: ${servicio.id}</li>
-                        <p> servicio: ${servicio.nombre}</p>
-                        <b> precio: ${servicio.precio}<b>`;
-document.body.appendChild (contenedor);
+let contenedor = document.getElementById(`containerTarjetas`);
+let cardTienda = document.createElement(`<div class="card-tienda"></div>`);
+
+cardTienda.innerHTML = `<div class="card-tienda__img" id="tienda${this.id}" style="background: url("${this.imagenURL}") no-repeat 50% 70% / cover" ></div>
+                            <div class="card-tienda__content">
+                                <ul class="card-tienda__list">
+                                    <li id="tituloTarjeta" class="card-tienda__item">Sesion de Psicologia</li>
+                                </ul>
+                                <p class="card-tienda__desc">${this.nombre}</p>
+                                <button id= "btnPrincipal" class= "btnComprar card-tienda__btn" href="">Comprar</button>
+                            </div>
+                        </div>`;
+
+contenedor.appendChild (cardTienda);
+
+
 
 // 2. Función para guardar en storage el JSON del array carrito
-function guardarLocalStorage(){
+function recuperarCarrito(){
 
-    let userName = "servicio";
-    let password = "pass";
-    let login = false;
+    sessionStorage.setItem (`guardarCarrito`,`carrito`)
+
+    let guardarCarrito = "carrito";
     
-    const carrito = []
-  
-    
-    localStorage.setItem ("userName", JSON.stringify(userName));
-    
+    sessionStorage.setItem ("guardarCarrito", JSON.stringify(guardarCarrito));
+
+    for (let i = 0; i < sessionStorage.length; i++){
+        let guardarCarrito = sessionStorage.key(i);
+        console.log ("guardarCarrito: " + guardarCarrito);
+        console.log ("carrito: " + JSON.parse(sessionStorage.getItem(guardarCarrito)));
     }
     
+    }
+
+
+let botonCarrito = document.getElementById ("btnPrincipal")
+boton.onclick = () => {crearItemCarrito()};
+
+function crearItemCarrito (){
+    if (botonCarrito == `${this.id}`){
+       console.log (`${this.id}` + 1);  
+    }else {
+        new ItemCarrito (id = `${this.id}`)
+    
+}
+};
+
+ItemCarrito.push (carrito);
+
+sessionStorage.setItem (`guardarCarrito`, JSON.stringify(guardarCarrito));
+
+
+
+/* Sesion buscar carrito, si hay carrito traerlo sino crearlo.
+funcion onclick:
+trae carrito guardado en sesion. se convierte para q se pueda usar.
+crear funcion crearItemCarrito (carrito) -
+busca si algun objeto en el carrito (if else) tiene el mismo id que el servicio desde que se hizo el click. (boton=id "boton +${this.id}").
+si esta, le modificas al que te trajo la cantidad (+1) y sino crea un new ItemCarrito pasandole id con el que hiciste la busqueda. 
+ en base a donde se produjo el click, lo pushea a carrito y vuelve a guardar en JSON carrito y guardarlo en Session Starge*/
 
